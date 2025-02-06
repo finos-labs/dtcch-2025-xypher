6043187c7aaaf064f4659fc6c75d5deef198e3fb
Amit Kumar Singh <aks12331@gmail.com>
2025-02-06 17:49:41 +0530

    cash flow prediction chart

--- Xypher-frontend/settlement-dashboard/src/pages/SettlementDashboard.tsx
+++ Xypher-frontend/settlement-dashboard/src/pages/SettlementDashboard.tsx
@@ -1,4 +1,5 @@
 import { CashFlowCard } from "@/components/CashFlowCard";
+import { CashFlowChart } from "@/components/CashflowPredictionsChart";
 import { TradeErrorsChart } from "@/components/ErrorDetectionTypes";
 import { FlaggedTradesCard } from "@/components/FlaggedTrades";
 import { HighValueTrades } from "@/components/HighValueTrades";
@@ -53,18 +54,18 @@ const SettlementDashboard = () => {
     return () => clearInterval(interval);
   }, [basePrice]);
   return (
-    <div className="flex flex-col min-w-[99vw] min-h-[100vh] py-3  overflow-y-auto overflow-x-hidden ">
-      <h1 className=" self-center px-8 py-2 mb-12 rounded-lg relative top-1 text-3xl font-bold tracking-tight text-primary self-center">
+    <div className="flex flex-col px-4 py-4 w-full min-h-[100vh] gap-4  overflow-y-auto  ">
+      <h1 className=" self-center px-8 py-8 mb-12 rounded-lg relative top-1 text-4xl font-bold tracking-tight text-primary self-center">
         SETTLEMENT DASHBOARD
       </h1>
-      <div className="flex flex-row gap-4 mb-6 w-[99vw] min-h-fit max-h-[10vh]">
+      <div className="flex flex-row gap-4 min-h-fit max-h-[10vh]">
         <CashFlowCard trend="up" title="Cash Inflow" />
         <CashFlowCard trend="down" title="Cash Outflow" />
         <FlaggedTradesCard />
         <HighValueTrades />
       </div>
-      <div className="flex flex-row gap-6 w-[99vw] min-h-[50vh] bg-background transition-colors">
-        <div className=" max-w-[100%] min-h-[40vh] max-h-[60vh]  overflow-auto rounded-lg border min-h-screen bg-background">
+      <div className="flex flex-row gap-4 w-full max-h-[60vh] bg-background transition-colors">
+        <div className="  grow-3 min-h-[40vh] max-h-[60vh]  overflow-auto rounded-lg border bg-background">
           <div className="container flex items-center px-4">
             <div className="flex items-center space-x-2">
               <Shield className="mt-6 h-6 w-6" />
@@ -73,18 +74,19 @@ const SettlementDashboard = () => {
               </h1>
             </div>
           </div>
-          <div className="container mx-auto py-8 px-4">
+          <div className="container mx-auto py-8 px-4 overflow-y-auto">
             <TradeTable />
           </div>
         </div>
-        <div className="flex rounded-lg border min-w-fit items-center justify-center p-4">
+        <div className=" grow-1 flex rounded-lg border  min-w-fit items-center justify-center p-4">
           <TradeSimulation bids={bids} asks={asks} spread={basePrice} />
         </div>
       </div>
-      <div className=" max-w-[100%] min-h-[40vh] max-h-[60vh]  overflow-auto rounded-lg border min-h-screen bg-background">
+      <div className=" max-w-[100%] min-h-[40vh] max-h-[60vh] px-6 overflow-auto rounded-lg border min-h-fit bg-background">
         <h1 className="mt-6 text-xl font-bold">AI Predcitions</h1>
         <div className="flex flex-row gap-5 my-6 min-h-fit max-h-[10vh]">
           <TradeErrorsChart />
+          <CashFlowChart />
         </div>
       </div>
     </div>
